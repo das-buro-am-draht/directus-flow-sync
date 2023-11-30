4be67ca0835b8f2f4e9c32bdb08cbdc97f92a3a9
philipp.schwarz <psz@dasburo.com>
2023-11-30 12:46:16 +0100

    chore: format code

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,15 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-    extensionsToTreatAsEsm: ['.ts'],
-    moduleNameMapper: {
-        '^(\\.{1,2}/.*)\\.js$': '$1',
-    },
-    transform: {
-        '^.+\\.tsx?$': [
-            'ts-jest',
-            {
-                useESM: true,
-            },
-        ],
-    },
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
 };
